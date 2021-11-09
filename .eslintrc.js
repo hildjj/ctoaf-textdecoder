31a7b392c04b6f943a5abd338a2263c402ece3e5
Joe Hildebrand <joe-github@cursive.net>
2021-11-09 08:22:02 -0700

    Fixes #1

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@
 
 module.exports = {
   root: true,
-  extends: '@cto.af'
+  extends: '@cto.af',
+  rules: {
+    // Old node versions
+    'prefer-template': 'off',
+    'node/no-unsupported-features/node-builtins': 'off',
+    'node/prefer-global/buffer': 'off',
+    'prefer-arrow-callback': 'off',
+  },
 }
