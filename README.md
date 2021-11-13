eaadd8eed56a8352c3e290a10a2d3d3de08baf80
Joe Hildebrand <joe-github@cursive.net>
2021-11-13 13:51:07 -0700

    Add support for the full TextDecoder API in the polyfill.  Make polyfill testable.  Fixes #3.

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # Simple TextDecoder polyfill
 
-I needed this in two different projects, so I pulled it out.  All of the existing TextDecoder polyfills try to do too much for what I needed.
+I needed this in two different projects, so I pulled it out.  All of the
+existing TextDecoder polyfills try to do too much for what I needed.  The goal
+here is to NOT require any Unicode tables, keeping this as small as possible.
 
-This just finds the best TextDecoder instance it can, and mocks in a dirty one for really old environments.
+This just finds the best TextDecoder instance it can, and mocks in a dirty one
+for old or broken environments.
 
 ## Use
 
 ```js
 const TextDecoder = require('@cto.af/textdecoder')
 ```
+
+## API
+
+See [MDN](https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder)
+
+[![Tests](https://github.com/hildjj/ctoaf-textdecoder/actions/workflows/node.js.yml/badge.svg)](https://github.com/hildjj/ctoaf-textdecoder/actions/workflows/node.js.yml)
--- index.js
+++ index.js
@@ -4,67 +4,106 @@ function add(t, v) {
   return t + v
 }
 
-let TD = null
-// Note: using globalThis is WAY more complicated than is needed.
-if (typeof TextDecoder === 'function') {
-  // This should be in most modern browsers, and node 11+
-  TD = TextDecoder
-} else if (typeof window !== 'undefined') {
-  // eslint-disable-next-line no-undef
-  TD = window.TextDecoder
-} else if (typeof self === 'undefined') {
-  try {
-    if (process &&
-        process.versions &&
-        process.versions.node &&
-        process.versions.node.match(/^(?:8|10)\./) &&
-        (typeof TD !== 'function')) {
-      // Let's see if we can fake out the metro bundler.
-      // The goal is to *not* try to inline the util package here.
-      const rq = require
-      const u = ['u', 't', 'i', 'l'].reduce(add, '')
-
-      // Node 8.3 - 10
-      // eslint-disable-next-line no-useless-call
-      const util = rq.call(null, u)
-      TD = util.TextDecoder
-    }
-  } catch (ignored) {
-    // Module util couldn't be loaded.  Probably an old browser.
+class TextDecoderPolyfill {
+  constructor(utfLabel, options) {
+    this.utfLabel = utfLabel || 'utf-8'
+    options = options || {}
+    this.fatal = Boolean(options.fatal)
+    this.ignoreBOM = Boolean(options.ignoreBOM)
+    this.buffer = null
   }
-} else {
-  // eslint-disable-next-line no-undef
-  TD = self.TextDecoder
+
+  decode(buf, options) {
+    // This isn't very good, but people should use more modern things
+    // so they don't need it.
+    const stream = Boolean(options && options.stream)
+    if (stream && this.buffer) {
+      buf = Buffer.concat([this.buffer, buf])
+      this.buffer = null
+    }
+    let str = buf.toString(this.utfLabel)
+    if (stream) {
+      if (str.codePointAt(str.length - 1) === 0xFFFD) {
+        // Truncated character at the end
+        str = str.slice(0, -1)
+        let start = buf.length - 1
+        while ((start >= 0) && (buf[start] & 0x80)) {
+          start--
+        }
+        this.buffer = buf.slice(start + 1)
+      }
+    }
+    if (this.fatal) {
+      for (const c of str) {
+        // The default Buffer.prototype.toString() implementation uses
+        // U+FFFD: REPLACEMENT CHARACTER
+        // for any bad UTF encoding.
+        if (c.codePointAt(0) === 0xFFFD) {
+          const err = new TypeError(
+            '[ERR_ENCODING_INVALID_ENCODED_DATA]: ' +
+            'The encoded data was not valid for encoding ' + this.utfLabel
+          )
+          err.code = 'ERR_ENCODING_INVALID_ENCODED_DATA'
+          err.errno = 12
+          throw err
+        }
+      }
+    }
+    if (!this.ignoreBOM) {
+      // U+FEFF: BYTE ORDER MARK
+      if (str.codePointAt(0) === 0xFEFF) {
+        return str.slice(1)
+      }
+    }
+    return str
+  }
+}
+
+const hasICU = (typeof Intl === 'object')
+let TD = null
+
+// Don't use built-in TextDecoder if in a node version without ICU support,
+// since the whole reason we're doing this is to allow `fatal: true`.
+// See: https://nodejs.org/api/util.html#util_new_textdecoder_encoding_options
+if (hasICU) {
+  // Note: using globalThis is WAY more complicated than is needed.
+  if (typeof TextDecoder === 'function') {
+    // This should be in most modern browsers, and node 11+
+    TD = TextDecoder
+  } else if (typeof window !== 'undefined') {
+    // eslint-disable-next-line no-undef
+    TD = window.TextDecoder
+  } else if (typeof self === 'undefined') {
+    try {
+      if (process &&
+          process.versions &&
+          process.versions.node &&
+          process.versions.node.match(/^(?:8|10)\./)) {
+        // Let's see if we can fake out the metro bundler.
+        // The goal is to *not* try to inline the util package here.
+        const rq = require
+        const u = ['u', 't', 'i', 'l'].reduce(add, '')
+
+        // Node 8.3 - 10
+        // eslint-disable-next-line no-useless-call
+        const util = rq.call(null, u)
+        TD = util.TextDecoder
+      }
+    } catch (ignored) {
+      // Module util couldn't be loaded.  Probably an old browser.
+    }
+  } else {
+    // eslint-disable-next-line no-undef
+    TD = self.TextDecoder
+  }
 }
 
 if (typeof TD !== 'function') {
-  class TextDecoder {
-    constructor(utfLabel, options) {
-      this.utfLabel = utfLabel
-      this.options = options
-    }
+  TD = TextDecoderPolyfill
+}
 
-    decode(buf) {
-      // This isn't very good, but people should use more modern things
-      // so they don't need it.
-      const str = buf.toString(this.utfLabel)
-      if (this.options.fatal) {
-        for (const c of str) {
-          // U+FFFD: REPLACEMENT CHARACTER
-          if (c.codePointAt(0) === 0xFFFD) {
-            const err = new TypeError(
-              '[ERR_ENCODING_INVALID_ENCODED_DATA]: ' +
-              'The encoded data was not valid for encoding ' + this.utfLabel
-            )
-            err.code = 'ERR_ENCODING_INVALID_ENCODED_DATA'
-            err.errno = 12
-            throw err
-          }
-        }
-      }
-      return str
-    }
-  }
-  TD = TextDecoder
+// Make polyfill testable on late-enough node versions
+if ((typeof Symbol === 'function') && (typeof Symbol.for === 'function')) {
+  TD[Symbol.for('@cto.af/textdecoder/polyfill')] = TextDecoderPolyfill
 }
 module.exports = TD
